dbdcae40b4f76c41d251033c43b904c93d933589
Eulogio Lopez <usuariogit@hotmail.com>
2020-06-18 16:54:50 +0200

    Update I2C_EEPROM_inopya.h

--- src/I2C_EEPROM_inopya.h
+++ src/I2C_EEPROM_inopya.h
@@ -5,13 +5,14 @@
  */
  
 #include <Wire.h>
+
 #ifndef I2C_EEPROM_inopya_h
 #define I2C_EEPROM_inopya_h
 
 #if defined(ARDUINO) && ARDUINO >= 100
-#include "Arduino.h"
+  #include "Arduino.h"
 #else
-#include "WProgram.h"
+  #include "WProgram.h"
 #endif
 
 class I2C_EEPROM_inopya
@@ -31,7 +32,7 @@ class I2C_EEPROM_inopya
 	template< typename T > T &save(unsigned int memory_pos, T informacion )
 	{
 	  uint8_t *ptr = (uint8_t*) &informacion;
-	  //Serial.print(F("save (grabando en EEPROM ")); Serial.println(informacion);
+	  //Serial.print(F("save, grabando en EEPROM ")); Serial.println(informacion);
 	  for(int count=0; count<sizeof(T); count++){
 		Wire.beginTransmission(_chipID); 
 		Wire.write((int)(memory_pos >> 8));   // MSB
@@ -39,7 +40,8 @@ class I2C_EEPROM_inopya
 		Wire.write(*ptr);
 		Wire.endTransmission();
 		//Serial.print(memory_pos); Serial.print(F(" --> "));Serial.println(dato);
-		delay(3);   // se recomiendan 5 ms, pero parece que con 3 es estable 
+		//delay(5);   // se recomiendan 5 ms, pero algunos modelos desde 3ms la consolidacion de datos es estable
+		delayMicroseconds(3500); // "jugar" con este valor si se desea acelerar un poco el proceso
 		ptr++;
 		memory_pos++;
 	  }
@@ -50,7 +52,7 @@ class I2C_EEPROM_inopya
 	/* parametros: 'Direccion de memoria (unsigned int)' de la que deseamos leer, variable (de cualquier tipo) para cargar el valor leido */
 	template< typename T > T &load(unsigned int memory_pos, T &informacion )
 	{
-	  //Serial.print(F("load (cargando de EEPROM "));Serial.print(sizeof(T));Serial.println(F(" bytes"));
+	  //Serial.print(F("load, cargando de EEPROM "));Serial.print(sizeof(T));Serial.println(F(" bytes"));
 	  uint8_t *ptr = (uint8_t*) &informacion;
 
 	  for(int count=0; count<sizeof(T); count++){
